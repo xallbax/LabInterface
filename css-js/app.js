04ca37b444e800eb4327eaaa1825b4cdcc186e08
xallbax <136092867+xallbax@users.noreply.github.com>
2024-10-16 15:37:51 +0400

    Add files via upload

---
+++ css-js/app.js
@@ -0,0 +1,30 @@
+﻿let tg = window.Telegram.WebApp;
+tg.expand();
+tg.MainButton.textColor = "#FFFFFF";
+tg.MainButton.color = "#2cab37";
+let item = "";
+let btn1 = document.getElementById("btn1");
+let btn2 = document.getElementById("btn2");
+btn1.addEventListener("click", function () {
+    if (tg.MainButton.isVisible) {
+        tg.MainButton.hide();
+    }
+    else {
+        tg.MainButton.setText("Вывести информацию по жанрам");
+        item = "1";
+        tg.MainButton.show();
+    }
+});
+btn2.addEventListener("click", function () {
+    if (tg.MainButton.isVisible) {
+        tg.MainButton.hide();
+    }
+    else {
+        tg.MainButton.setText("Вывести информацию по исполнителям");
+        item = "2";
+        tg.MainButton.show();
+    }
+});
+Telegram.WebApp.onEvent("mainButtonClicked", function () {
+    tg.sendData(item);
+});
